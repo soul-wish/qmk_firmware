ac93145bb2bb2e5234919d54ad1b576fae81ca59
Sergii Lysenko <soulwish.ls@gmail.com>
2024-09-17 07:16:08 -0400

    fix encoder behaviour

--- keyboards/cheapino/encoder.c
+++ keyboards/cheapino/encoder.c
@@ -16,14 +16,10 @@ void clicked(void) {
 }
 
 void turned(bool clockwise) {
-    if (IS_LAYER_ON(6)) {
-        tap_code(clockwise ? KC_VOLU : KC_VOLD);
-    } else if (IS_LAYER_ON(3)) {
-        tap_code16(clockwise ? LCTL(KC_TAB) : LCTL(LSFT(KC_TAB)));
-    } else if (IS_LAYER_ON(5)) {
-        tap_code16(clockwise ? LGUI(KC_Y) : LGUI(KC_Z));
+    if (IS_LAYER_ON(0)) {
+        tap_code16(clockwise ? KC_WH_D : KC_WH_U);
     } else {
-        tap_code16(clockwise ? KC_PGDN : KC_PGUP);
+        tap_code(clockwise ? KC_VOLU : KC_VOLD);
     }
 }
 
